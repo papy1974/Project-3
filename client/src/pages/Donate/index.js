17aa88786f3182474941d82fe533c73bb4a4b341
Drake <drakedemuyt@gmail.com>
2020-07-12 20:31:29 -0400

    Adding LI component and API functionality on Donante page with on search commands and hooks

--- client/src/pages/Donate/index.js
+++ client/src/pages/Donate/index.js
@@ -1,21 +1,54 @@
-import React from "react";
-import { Container, Row } from 'react-bootstrap';
+import React, { useState } from "react";
+import { Container, Row } from "react-bootstrap";
 import "./donate.css";
-import { searchApi } from "../../utils/API";
 import API from "../../utils/API";
+import LI from "../../components/LI/index";
 
 function Donate() {
-  
+  const [charities, setCharities] = useState([]);
+  const [search, setSearch] = useState("");
+  const inputEl = React.useRef();
+
+  function handleClick () {
+    {
+      API.searchApi({ body: search }).then((res) => {
+        const result = res.data;
+        console.log(result);
+        setCharities(result);
+        })
+        .catch(err => console.log(err))
+
+    }
+  }
+
   return (
     <Container>
       <Row>
         <p>Donate Page!</p>
-        <input type="search" placeholder="Charity Search" id="search"/>
-        <button onClick={(e => {
-          const search = document.getElementById("search").val();
-          API.searchApi(search);
-        })}>Search</button>
-      </Row>
+        </Row>
+        <Row>
+          <input
+          type="search"
+          placeholder="Charity Search"
+          ref={inputEl}
+          onChange={() => {
+            setSearch(inputEl.current.value.trim());
+            console.log(search);
+          }}
+        />
+        
+        <button onClick={() => handleClick()}>Search</button>
+       </Row>
+       <Row>
+        <ul>
+          {charities.map((charity) => (
+            <LI
+            key={charity.ein}
+            charityName={charity.charityName}
+            />
+          ))}
+        </ul>
+        </Row>
     </Container>
   );
 }
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,30 +1,28 @@
 import axios from "axios";
 
 export default {
-  // Gets all books
-  getBooks: function() {
-    return axios.get("/api/books");
-  },
-  // Gets the book with the given id
-  getBook: function(id) {
-    return axios.get("/api/books/" + id);
-  },
-  // Deletes the book with the given id
-  deleteBook: function(id) {
-    return axios.delete("/api/books/" + id);
-  },
-  // Saves a book to the database
-  saveBook: function(bookData) {
-    return axios.post("/api/books", bookData);
-  },
+  // // Gets all books
+  // getBooks: function() {
+  //   return axios.get("/api/books");
+  // },
+  // // Gets the book with the given id
+  // getBook: function(id) {
+  //   return axios.get("/api/books/" + id);
+  // },
+  // // Deletes the book with the given id
+  // deleteBook: function(id) {
+  //   return axios.delete("/api/books/" + id);
+  // },
+  // // Saves a book to the database
+  // saveBook: function(bookData) {
+  //   return axios.post("/api/books", bookData);
+  // },
     // function to use API from backend
   searchApi: (req, res) => {
 
-    // userSearch = `&search=children`
+    let userSearch = req.body;
+    let url = `https://api.data.charitynavigator.org/v2/Organizations?app_id=f591e4ff&app_key=0d693d8ab79cff6b8e20b4a8c6896c0a&rated=true&search=`
 
-    axios.get("https://api.data.charitynavigator.org/v2/Organizations?app_id=f591e4ff&app_key=0d693d8ab79cff6b8e20b4a8c6896c0a&rated=true&search=children")
-        .then((response) => res.json(response.data))
-        .catch(err => res.status(422).json(err))
-
+    return axios.get(url + userSearch);
 },
 };
