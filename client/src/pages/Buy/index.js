0f6078eb0bc6187207a7e4730e7713d813b0196f
Drake <drakedemuyt@gmail.com>
2020-07-15 21:05:39 -0400

    Adding design and using hooks to implement db data on nav buy

--- client/src/pages/Buy/index.js
+++ client/src/pages/Buy/index.js
@@ -7,7 +7,7 @@ import "./buy.css";
 
 function Buy() {
   
-  const [products, setProducts] = useState([{key: 1, name: 'Vase', price: 200, desc: 'Decorative Vase for Charity'}]);
+  const [products, setProducts] = useState([{key: 1, name: 'Vase', img: '/example.vase.png', price: 200, desc: 'Decorative Vase for Charity'}, {key: 1, name: 'Vase', img: '/example.vase.png', price: 200, desc: 'Decorative Vase for Charity'}]);
 
   return (
     <Container>
@@ -18,6 +18,7 @@ function Buy() {
         {products.map((product) => (
           <ProductCard
           key={product.key}
+          img={product.img}
           name={product.name}
           price={product.price}
           desc={product.desc}
